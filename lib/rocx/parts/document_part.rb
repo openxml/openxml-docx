5ac0bf93bf5f7187ea35e87c445abff42f0b6f56
Gene Doyel <gene.doyel@cph.org>
2013-01-02 11:21:17 -0600

    [fix] Added additional namespaces to document.xml

--- lib/rocx/parts/document_part.rb
+++ lib/rocx/parts/document_part.rb
@@ -16,8 +16,27 @@ module Rocx
       @path = 'word/document.xml'
       @document = XML::Document.new()
       @document.root = XML::Node.new('document')
-      @namespace = XML::Namespace.new(@document.root, 'w', "http://schemas.openxmlformats.org/wordprocessingml/2006/main")
-      @body = XML::Node.new('body', nil, @namespace)
+      @w_namespace = XML::Namespace.new(@document.root, 'w', "http://schemas.openxmlformats.org/wordprocessingml/2006/main")
+      @document.root.namespaces.namespace = @w_namespace
+      XML::Namespace.new(@document.root, "wpc", "http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas")
+      XML::Namespace.new(@document.root, "mo", "http://schemas.microsoft.com/office/mac/office/2008/main")
+      @mc_namespace = XML::Namespace.new(@document.root, "mc", "http://schemas.openxmlformats.org/markup-compatibility/2006")
+      XML::Namespace.new(@document.root, "mv", "urn:schemas-microsoft-com:mac:vml")
+      XML::Namespace.new(@document.root, "o", "urn:schemas-microsoft-com:office:office")
+      XML::Namespace.new(@document.root, "r", "http://schemas.openxmlformats.org/officeDocument/2006/relationships")
+      XML::Namespace.new(@document.root, "m", "http://schemas.openxmlformats.org/officeDocument/2006/math")
+      XML::Namespace.new(@document.root, "v", "urn:schemas-microsoft-com:vml")
+      XML::Namespace.new(@document.root, "wp14", "http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing")
+      XML::Namespace.new(@document.root, "wp", "http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing")
+      XML::Namespace.new(@document.root, "w10", "urn:schemas-microsoft-com:office:word")
+      XML::Namespace.new(@document.root, "w14", "http://schemas.microsoft.com/office/word/2010/wordml")
+      XML::Namespace.new(@document.root, "wpg", "http://schemas.microsoft.com/office/word/2010/wordprocessingGroup")
+      XML::Namespace.new(@document.root, "wpi", "http://schemas.microsoft.com/office/word/2010/wordprocessingInk")
+      XML::Namespace.new(@document.root, "wne", "http://schemas.microsoft.com/office/word/2006/wordml")
+      XML::Namespace.new(@document.root, "wps", "http://schemas.microsoft.com/office/word/2010/wordprocessingShape")
+      ignorable = XML::Attr.new(@document.root, "Ignorable", "w14 wp14")
+      ignorable.namespaces.namespace = @mc_namespace
+      @body = XML::Node.new('body', nil, @w_namespace)
       @document.root << @body
       
       @children = []
@@ -26,7 +45,7 @@ module Rocx
     end
     
     def to_xml
-      xr = XmlRenderer.new(namespace)
+      xr = XmlRenderer.new(@w_namespace)
       xr.render_tree(self)
     end
     
