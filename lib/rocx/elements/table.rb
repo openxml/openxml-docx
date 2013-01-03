48699fa5e0c91a7e77c253a694979b80f9f3c350
Gene Doyel <gene.doyel@cph.org>
2013-01-03 10:31:17 -0600

    [improvement] added borders to table, with border color and style as options

--- lib/rocx/elements/table.rb
+++ lib/rocx/elements/table.rb
@@ -12,6 +12,9 @@ module Rocx
         @column_count = contents[0].length
         @column_width = @width / @column_count # ?
         @contents = contents
+        @has_borders = options[:borders] || false
+        @border_type = options[:border_type] || 'single'
+        @border_color = options[:border_color] || 'auto'
       end
       
       def to_xml(namespace)
@@ -21,25 +24,34 @@ module Rocx
           properties = make_element 'tblPr'
           style = make_element 'tblStyle', attributes: {'val' => ''}
           properties << style
-        
+          
           width = make_element 'tblW', attributes: {'w' => @width.to_s, 'type' => @width_unit.to_s}
           properties << width
-        
-          # !todo: add borders :P
-        
+          
+          # !todo: make borders better
+          if @has_borders            
+            border_types = ['top', 'left', 'bottom', 'right', 'insideH', 'insideV']
+            borders = make_element 'tblBorders'
+            border_types.each do |type|
+              border_element = make_element type, attributes: {'color' => @border_color, 'space' => "0", 'sz' => "4", "val" => @border_type}
+              borders << border_element
+            end
+            properties << borders
+          end   # tables sans frontieres
+                    
           look = make_element 'look', attributes: {'val' => '0400'}
           properties << look
           table << properties
-        
+          
           # grid
           grid = make_element 'tblGrid'
           (0...@column_count).each do |i|
             grid << make_element('gridCol', attributes: {'w' => @column_width.to_s})
           end
           table << grid
-        
+          
           # !todo: add heading row
-        
+          
           # contents
           @contents.each do |content_row|
             row = make_element 'tr'
