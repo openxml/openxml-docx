ddce6e1c863fe85f026c1ba5714e38b99b7e19d1
Luke Booth <Luke.Booth@cph.org>
2013-01-03 14:52:58 -0600

    [refactor] Renamed document_part to document

--- test/document_test.rb
+++ test/document_test.rb
@@ -1,38 +1,19 @@
-# require 'test_helper'
-# 
-# class DocumentTest < ActiveSupport::TestCase
-#   
-#   test "calling save on a document should create the word file" do
-#     document = Rocx::Document.new do
-#       body do
-#         title "Page Title"
-#         paragraph "This is a test page created by Rocx"
-#         
-#         heading "Lets look at some bullets with varying levels"
-#         bullet "Red", level: 0
-#         bullet "Green", level: 0
-#         bullet "Yellow", level: 1
-#         bullet "Black", level: 2
-#         bullet "Orange", level: 0
-#         
-#         paragraph "How about we start fresh on a new page?"
-#         page_break
-#         
-#         heading "Names & Positions"
-#         table [
-#           ["Luke", "Programmer"],
-#           ["Gene", "Programmer"],
-#           ["Jesse", "Programmer"],
-#           ["Bob", "Boss"]
-#         ]
-#         
-#         paragraph "That's it so far!"
-#       end
-#     end
-#     file_path = File.expand_path('~/Desktop/test.docx')
-#     document.save(file_path)
-#     assert File.file?(file_path)
-#     `open #{file_path}`
-#   end
-#   
-# end
+require 'test_helper'
+
+class DocumentTest < ActiveSupport::TestCase
+  
+  test "document to_s should print out the xml document" do
+    document = Rocx::Document.new do
+      body
+    end
+    document.document.to_xml
+    expected = <<-XML
+<?xml version="1.0" encoding="UTF-8"?>
+<w:document xmlns:wpc="http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas" xmlns:mo="http://schemas.microsoft.com/office/mac/office/2008/main" xmlns:mv="urn:schemas-microsoft-com:mac:vml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:wp14="http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing" xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:w10="urn:schemas-microsoft-com:office:word" xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml" xmlns:wpg="http://schemas.microsoft.com/office/word/2010/wordprocessingGroup" xmlns:wpi="http://schemas.microsoft.com/office/word/2010/wordprocessingInk" xmlns:wne="http://schemas.microsoft.com/office/word/2006/wordml" xmlns:wps="http://schemas.microsoft.com/office/word/2010/wordprocessingShape" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" mc:Ignorable="w14 wp14">
+  <w:body/>
+</w:document>
+    XML
+    assert_equal expected, document.document.to_s
+  end
+  
+end
