c5ddb76ba3eac1d6bcd32a7a0bef5d7c4616c5e1
Gene Doyel <gene.doyel@cph.org>
2014-03-07 12:36:14 -0600

    [skip] Added helper for asserting that an instance is a class (1m)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,3 +10,7 @@ SimpleCov.start
 def part_xml(part)
   File.read(File.join(File.dirname(__FILE__), "data", "parts", "#{part}_part.xml"))
 end
+
+def assert_is_a(klass, instance)
+  assert instance.is_a?(klass), "Expected #{instance} to be a #{klass}"
+end
