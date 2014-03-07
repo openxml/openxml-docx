da70602fe4ae4a4f5a4684a9dbefc407c2dad6f1
Gene Doyel <gene.doyel@cph.org>
2014-03-07 12:56:52 -0600

    [skip] Added rels part (10m)

--- lib/rocx/package.rb
+++ lib/rocx/package.rb
@@ -1,10 +1,13 @@
 module Rocx
   class Package
 
-    attr_reader :core, :web_settings
+    attr_reader :core,
+                :rels,
+                :web_settings
 
     def initialize
       @core = Rocx::Parts::Core.new
+      @rels = Rocx::Parts::Rels.new
       @web_settings = Rocx::Parts::WebSettings.new
     end
 
---
+++ test/parts/rels_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class RelsTest < Test::Unit::TestCase
+  attr_reader :rels
+
+  context "transforming to XML" do
+    should "output the correct XML" do
+      @rels = Rocx::Parts::Rels.new
+      assert_equal part_xml('rels'), @rels.to_xml
+    end
+  end
+
+end
