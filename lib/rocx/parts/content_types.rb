38bf4021d1cb3c411ec742660985829bf3cbae07
Gene Doyel <gene.doyel@cph.org>
2014-05-05 10:21:39 -0500

    [refactor] Removed unnecessary defaults and overrides from ContentTypes (20m)

--- lib/rocx/parts/content_types.rb
+++ lib/rocx/parts/content_types.rb
@@ -1,35 +1,41 @@
 module Rocx
   module Parts
     class ContentTypes < BasePart
+      attr_reader :defaults, :overrides
+
+      def initialize
+        @defaults, @overrides = [], []
+        install_preset_defaults
+        install_preset_overrides
+      end
+
+      def install_preset_defaults
+        default "xml", "application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml"
+        default "rels", "application/vnd.openxmlformats-package.relationships+xml"
+        default "png", "image/png"
+      end
+
+      def install_preset_overrides
+        override "/word/styles.xml", "application/vnd.openxmlformats-officedocument.wordprocessingml.styles+xml"
+        override "/word/settings.xml", "application/vnd.openxmlformats-officedocument.wordprocessingml.settings+xml"
+      end
+
+      def default(extension, content_type)
+        defaults << {"Extension" => extension, "ContentType" => content_type}
+      end
+
+      def override(part_name, content_type)
+        overrides << {"PartName" => part_name, "ContentType" => content_type}
+      end
 
       def to_xml
-        build_xml do |xml|
+        xml = build_xml do |xml|
           xml.Types(xmlns: "http://schemas.openxmlformats.org/package/2006/content-types") {
             defaults.each { |default| xml.Default(default) }
             overrides.each { |override| xml.Override(override) }
           }
         end
-      end
-
-    private
-
-      def defaults
-        [ {"Extension" => "xml", "ContentType" => "application/xml" },
-          {"Extension" => "rels", "ContentType" => "application/vnd.openxmlformats-package.relationships+xml" },
-          {"Extension" => "jpeg", "ContentType" => "image/jpeg" } ]
-      end
-
-      def overrides
-        [ {"PartName" => "/word/document.xml", "ContentType" => "application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml"},
-          {"PartName" => "/word/numbering.xml", "ContentType" => "application/vnd.openxmlformats-officedocument.wordprocessingml.numbering+xml"},
-          {"PartName" => "/word/styles.xml", "ContentType" => "application/vnd.openxmlformats-officedocument.wordprocessingml.styles+xml"},
-          {"PartName" => "/word/stylesWithEffects.xml", "ContentType" => "application/vnd.ms-word.stylesWithEffects+xml"},
-          {"PartName" => "/word/settings.xml", "ContentType" => "application/vnd.openxmlformats-officedocument.wordprocessingml.settings+xml"},
-          {"PartName" => "/word/webSettings.xml", "ContentType" => "application/vnd.openxmlformats-officedocument.wordprocessingml.webSettings+xml"},
-          {"PartName" => "/word/fontTable.xml", "ContentType" => "application/vnd.openxmlformats-officedocument.wordprocessingml.fontTable+xml"},
-          {"PartName" => "/word/theme/theme1.xml", "ContentType" => "application/vnd.openxmlformats-officedocument.theme+xml"},
-          {"PartName" => "/docProps/core.xml", "ContentType" => "application/vnd.openxmlformats-package.core-properties+xml"},
-          {"PartName" => "/docProps/app.xml", "ContentType" => "application/vnd.openxmlformats-officedocument.extended-properties+xml"} ]
+        strip_whitespace(xml)
       end
 
     end
--- test/parts/content_types_test.rb
+++ test/parts/content_types_test.rb
@@ -1,12 +1,37 @@
-require 'test_helper'
+require "test_helper"
 
 class ContentTypesTest < Test::Unit::TestCase
   attr_reader :content_types
 
+  context "when first creating the content types, it" do
+    setup do
+      @content_types = Rocx::Parts::ContentTypes.new
+    end
+
+    should "install the preset defaults" do
+      assert_equal 3, content_types.defaults.length, "Expected the number of defaults generated to match the number of preset defaults"
+    end
+
+    should "install the preset overrides" do
+      assert_equal 2, content_types.overrides.length, "Expected the number of overrides generated to match the number of preset overrides"
+    end
+  end
+
+  context "after initially creating the content types, it" do
+    setup do
+      @content_types = Rocx::Parts::ContentTypes.new
+    end
+
+    should "be able to add additional defaults" do
+      content_types.default "coolml", "text/coolml"
+      assert content_types.defaults.member?({"Extension" => "coolml", "ContentType" => "text/coolml"})
+    end
+  end
+
   context "transforming to XML" do
     should "output the correct XML" do
       @content_types = Rocx::Parts::ContentTypes.new
-      assert_equal part_xml('content_types'), content_types.to_xml
+      assert_equal part_xml('content_types').strip, content_types.to_xml
     end
   end
 end
