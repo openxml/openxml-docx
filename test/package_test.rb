21727e2d15a07abca71642eeabb4012632476c3a
Gene Doyel <gene.doyel@cph.org>
2014-05-05 13:12:33 -0500

    [skip] Added GlobalRels part (10m)

--- test/package_test.rb
+++ test/package_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class PackageTest < Test::Unit::TestCase
   attr_reader :package
 
-  context "when starting a new package" do
+  context "when starting a new package, it" do
     setup do
       @package = Rocx::Package.new
     end
@@ -14,6 +14,10 @@ class PackageTest < Test::Unit::TestCase
 
     should "create the document part" do
       assert_is_a Rocx::Parts::Document, package.document
+    end
+
+    should "create the global rels part" do
+      assert_is_a Rocx::Parts::GlobalRels, package.global_rels
     end
 
     should "create the _rels part" do
