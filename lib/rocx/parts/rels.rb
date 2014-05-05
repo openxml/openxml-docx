8c21aca1ea6bdc7e900bd1118a3f3123d5cbb4dd
Gene Doyel <gene.doyel@cph.org>
2014-05-05 10:38:44 -0500

    [refactor] Removed extra relationships and added more tests (10m)

--- lib/rocx/parts/rels.rb
+++ lib/rocx/parts/rels.rb
@@ -1,32 +1,46 @@
+require "securerandom"
+
 module Rocx
   module Parts
     class Rels < BasePart
       attr_reader :relationships
 
       def initialize
-        @relationships = %w{numbering styles settings webSettings fontTable theme}
+        @relationships = []
+        install_defaults
+      end
+
+      def install_defaults
+        defaults.each do |default|
+          relationship *default.values
+        end
+      end
+
+      def relationship(type, target, id=nil)
+        id = "R#{SecureRandom.hex}" unless id
+        relationships << {"Type" => type, "Target" => target, "Id" => id}
       end
 
       def to_xml
-        build_xml do |xml|
+        xml = build_xml do |xml|
           xml.Relationships(xmlns: "http://schemas.openxmlformats.org/package/2006/relationships") {
-            relationship_attributes.each do |rel|
+            relationships.each do |rel|
               xml.Relationship(rel)
             end
           }
         end
+        strip_whitespace(xml)
       end
 
     private
 
-      def relationship_attributes
-        relationships.each_with_index.map do |rel, index|
-          attrs = {
-            "Id" => "rId#{index + 1}",
-            "Type" => "http://schemas.openxmlformats.org/officeDocument/2006/relationships/#{rel}",
-            "Target" => rel == "theme" ? "theme/theme1.xml" : "#{rel}.xml"
-          }
-        end
+      def defaults
+        [{ type: "http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles",
+           target: "/word/styles.xml",
+           id: "Rf06246a95c004384" },
+         { type: "http://schemas.openxmlformats.org/officeDocument/2006/relationships/settings",
+           target: "/word/settings.xml",
+           id: "R1efaa81aac7a4831" }]
       end
 
     end
