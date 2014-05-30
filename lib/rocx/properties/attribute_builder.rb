de4e9df603a6d447407dd167a6f0839e67cf1ed3
Gene Doyel <gene.doyel@cph.org>
2014-05-30 09:21:05 -0500

    [refactor] Extracted attribute logic to a module (5m)

---
+++ lib/rocx/properties/attribute_builder.rb
@@ -0,0 +1,58 @@
+module Rocx
+  module Properties
+    module AttributeBuilder
+
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+
+      module ClassMethods
+        def attribute(name, expects: nil, displays_as: nil)
+          attr_reader name
+
+          define_method "#{name}=" do |value|
+            send(expects, name, value)
+            instance_variable_set "@#{name}", value
+          end
+
+          attributes << name
+        end
+
+        def attributes
+          @attributes ||= []
+        end
+      end
+
+      def render?
+        attributes.map(&method(:send)).any?
+      end
+
+      def attributes
+        self.class.attributes
+      end
+
+    private
+
+      def true_or_false(name, value)
+        message = "Invalid #{name}: frame must be true or false"
+        raise ArgumentError, message unless [true, false].member? value
+      end
+
+      def hex_color(name, value)
+        message = "Invalid #{name}: must be :auto or a hex color, e.g. 4F1B8C"
+        raise ArgumentError, message unless value == :auto || value =~ /[0-9A-F]{6}/
+      end
+
+      def hex_digit(name, value)
+        message = "Invalid #{name}: must be a two-digit hex number, e.g. BF"
+        raise ArgumentError, message unless value =~ /[0-9A-F]{2}/
+      end
+
+      def positive_integer(name, value)
+        message = "Invalid #{name}: must be a positive integer"
+        raise ArgumentError, message unless value.is_a?(Integer) && value >= 0
+      end
+
+    end
+  end
+end
--- lib/rocx/properties/border.rb
+++ lib/rocx/properties/border.rb
@@ -1,6 +1,8 @@
 module Rocx
   module Properties
     class Border
+      include AttributeBuilder
+
       attr_reader :tag
 
       VALID_THEME_COLORS = %i(accent1 accent2 accent3 accent4 accent5 accent6 background1 background2 dark1 dark2 followedHyperlink hyperlink light1 light2 none text1 text2)
@@ -199,23 +201,6 @@ module Rocx
                        zigZag
                        zigZagStitch)
 
-      class << self
-        def attribute(name, expects: nil, displays_as: nil)
-          attr_reader name
-
-          define_method "#{name}=" do |value|
-            send(expects, name, value)
-            instance_variable_set "@#{name}", value
-          end
-
-          attributes << name
-        end
-
-        def attributes
-          @attributes ||= []
-        end
-      end
-
       attribute :color, expects: :hex_color
       attribute :frame, expects: :true_or_false
       attribute :shadow, expects: :true_or_false
@@ -232,10 +217,6 @@ module Rocx
 
       def name
         "border"
-      end
-
-      def render?
-        attributes.map(&method(:send)).any?
       end
 
       def to_xml(xml)
@@ -258,25 +239,6 @@ module Rocx
         attrs
       end
 
-      def attributes
-        self.class.attributes
-      end
-
-      def hex_color(name, value)
-        message = "Invalid #{name}: must be :auto or a hex color, e.g. 4F1B8C"
-        raise ArgumentError, message unless value == :auto || value =~ /[0-9A-F]{6}/
-      end
-
-      def hex_digit(name, value)
-        message = "Invalid #{name}: must be a two-digit hex number, e.g. BF"
-        raise ArgumentError, message unless value =~ /[0-9A-F]{2}/
-      end
-
-      def positive_integer(name, value)
-        message = "Invalid #{name}: must be a positive integer"
-        raise ArgumentError, message unless value.is_a?(Integer) && value >= 0
-      end
-
       def valid_theme_color(name, value)
         message = "Invalid #{name}: must be one of #{VALID_THEME_COLORS.join(", ")}"
         raise ArgumentError, message unless VALID_THEME_COLORS.member? value
@@ -285,11 +247,6 @@ module Rocx
       def valid_type(name, value)
         message = "Invalid #{name}: must be one of #{VALID_TYPES.join(", ")}"
         raise ArgumentError, message unless VALID_TYPES.member? value
-      end
-
-      def true_or_false(name, value)
-        message = "Invalid #{name}: frame must be true or false"
-        raise ArgumentError, message unless [true, false].member? value
       end
 
     end
