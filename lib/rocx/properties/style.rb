5007414552b9550693d468716985595d79f7a2df
Gene Doyel <gene.doyel@cph.org>
2014-06-07 08:16:25 -0500

    [skip] Moved file that was in the wrong place (1m)

--- lib/rocx/properties/style.rb
+++ lib/rocx/properties/style.rb
@@ -10,6 +10,10 @@ module Rocx
         :pStyle
       end
 
+      def invalid_message
+        "Invalid #{name}: value must not be nil"
+      end
+
       def to_xml(xml)
         xml["w"].public_send(tag, "w:val" => value)
       end
