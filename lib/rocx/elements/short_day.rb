507161dbb4db1bfd58e84ea2a524f7ad081b239e
Gene Doyel <gene.doyel@cph.org>
2014-06-13 11:16:27 -0500

    [improvement] Added the shortDay element (2m) [#19g]

---
+++ lib/rocx/elements/short_day.rb
@@ -0,0 +1,8 @@
+module Rocx
+  module Elements
+    class ShortDay < Element
+      tag :dayShort
+      
+    end
+  end
+end
---
+++ spec/elements/short_day_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+describe Rocx::Elements::ShortDay do
+  include ElementTestMacros
+
+  it_should_use tag: :dayShort, name: "short_day"
+
+  with_no_attributes_set do
+    it_should_output "<w:dayShort/>", assign: false
+  end
+
+end
