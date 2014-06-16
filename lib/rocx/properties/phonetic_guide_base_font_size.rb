ef25bdb4a1cbabb46f4f00b30c4ab83ad46bd946
Gene Doyel <gene.doyel@cph.org>
2014-06-16 09:10:17 -0500

    [improvement] Added the hpsBaseText property (10m) [#19l]

---
+++ lib/rocx/properties/phonetic_guide_base_font_size.rb
@@ -0,0 +1,8 @@
+module Rocx
+  module Properties
+    class PhoneticGuideBaseFontSize < PositiveIntegerProperty
+      tag :hpsBaseText
+      name "base_font_size"
+    end
+  end
+end
---
+++ spec/properties/phonetic_guide_base_font_size_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+describe Rocx::Properties::PhoneticGuideBaseFontSize do
+  include ValuePropertyTestMacros
+
+  it_should_use tag: :hpsBaseText, name: "base_font_size", value: 1
+
+  with_value(24) do
+    it_should_work
+    it_should_output "<w:hpsBaseText w:val=\"24\"/>"
+  end
+
+  with_value(-24) do
+    it_should_not_work
+  end
+
+  with_value(:big) do
+    it_should_not_work
+  end
+
+  with_value(23.3) do
+    it_should_not_work
+  end
+
+end
