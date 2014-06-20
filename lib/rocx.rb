43221bebfda369d96d1f15e238f8006bf6f28494
Gene Doyel <gene.doyel@cph.org>
2014-06-20 13:04:49 -0500

    [refactor] Extracted PropertyBuilder (10m)

--- lib/rocx.rb
+++ lib/rocx.rb
@@ -1,8 +1,10 @@
 require "rocx/attribute_builder"
+require "rocx/property_builder"
 require "rocx/properties"
 require "rocx/elements"
 require "rocx/package"
 require "rocx/parts"
+require "rocx/section"
 require "rocx/style"
 
 module Rocx
