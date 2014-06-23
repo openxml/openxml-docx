9ebf0099d017b9d8e3774e47162a446ab0b6a781
Gene Doyel <gene.doyel@cph.org>
2014-06-23 11:22:03 -0500

    [improvement] Added page margins to section properties (10m) [#23i]

---
+++ lib/rocx/properties/page_margins.rb
@@ -0,0 +1,15 @@
+module Rocx
+  module Properties
+    class PageMargins < ComplexProperty
+      tag :pgMar
+
+      attribute :bottom, expects: :integer
+      attribute :footer, expects: :positive_integer
+      attribute :gutter, expects: :positive_integer
+      attribute :header, expects: :positive_integer
+      attribute :left, expects: :positive_integer
+      attribute :right, expects: :positive_integer
+      attribute :top, expects: :integer
+    end
+  end
+end
---
+++ spec/properties/page_margins_spec.rb
@@ -0,0 +1,150 @@
+require "spec_helper"
+
+describe Rocx::Properties::PageMargins do
+  include PropertyTestMacros
+
+  it_should_use tag: :pgMar, name: "page_margins"
+
+  for_attribute(:bottom) do
+    with_value(1440) do
+      it_should_assign_successfully
+      it_should_output "<w:pgMar w:bottom=\"1440\"/>"
+    end
+
+    with_value(-1440) do
+      it_should_assign_successfully
+      it_should_output "<w:pgMar w:bottom=\"-1440\"/>"
+    end
+
+    with_value(9832.1) do
+      it_should_raise_an_exception
+    end
+
+    with_value(:big) do
+      it_should_raise_an_exception
+    end
+  end
+
+  for_attribute(:footer) do
+    with_value(1440) do
+      it_should_assign_successfully
+      it_should_output "<w:pgMar w:footer=\"1440\"/>"
+    end
+
+    with_value(-1440) do
+      it_should_raise_an_exception
+    end
+
+    with_value(9832.1) do
+      it_should_raise_an_exception
+    end
+
+    with_value(:big) do
+      it_should_raise_an_exception
+    end
+  end
+
+  for_attribute(:gutter) do
+    with_value(1440) do
+      it_should_assign_successfully
+      it_should_output "<w:pgMar w:gutter=\"1440\"/>"
+    end
+
+    with_value(-1440) do
+      it_should_raise_an_exception
+    end
+
+    with_value(9832.1) do
+      it_should_raise_an_exception
+    end
+
+    with_value(:big) do
+      it_should_raise_an_exception
+    end
+  end
+
+  for_attribute(:header) do
+    with_value(1440) do
+      it_should_assign_successfully
+      it_should_output "<w:pgMar w:header=\"1440\"/>"
+    end
+
+    with_value(-1440) do
+      it_should_raise_an_exception
+    end
+
+    with_value(9832.1) do
+      it_should_raise_an_exception
+    end
+
+    with_value(:big) do
+      it_should_raise_an_exception
+    end
+  end
+
+  for_attribute(:left) do
+    with_value(1440) do
+      it_should_assign_successfully
+      it_should_output "<w:pgMar w:left=\"1440\"/>"
+    end
+
+    with_value(-1440) do
+      it_should_raise_an_exception
+    end
+
+    with_value(9832.1) do
+      it_should_raise_an_exception
+    end
+
+    with_value(:big) do
+      it_should_raise_an_exception
+    end
+  end
+
+  for_attribute(:right) do
+    with_value(1440) do
+      it_should_assign_successfully
+      it_should_output "<w:pgMar w:right=\"1440\"/>"
+    end
+
+    with_value(-1440) do
+      it_should_raise_an_exception
+    end
+
+    with_value(9832.1) do
+      it_should_raise_an_exception
+    end
+
+    with_value(:big) do
+      it_should_raise_an_exception
+    end
+  end
+
+  for_attribute(:top) do
+    with_value(1440) do
+      it_should_assign_successfully
+      it_should_output "<w:pgMar w:top=\"1440\"/>"
+    end
+
+    with_value(-1440) do
+      it_should_assign_successfully
+      it_should_output "<w:pgMar w:top=\"-1440\"/>"
+    end
+
+    with_value(9832.1) do
+      it_should_raise_an_exception
+    end
+
+    with_value(:big) do
+      it_should_raise_an_exception
+    end
+  end
+
+  with_no_attributes_set do
+    it_should_output "", assign: false
+  end
+
+  with_these_attributes_set(header: 720, bottom: 1440, top: 1440, right: 1440, left: 1440, footer: 720, gutter: 0) do
+    it_should_output "<w:pgMar w:bottom=\"1440\" w:footer=\"720\" w:gutter=\"0\" w:header=\"720\" w:left=\"1440\" w:right=\"1440\" w:top=\"1440\"/>", assign: false
+  end
+end
