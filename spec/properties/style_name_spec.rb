dc0ab93b178b6fb93d91d7654f2f47c9368c8667
Gene Doyel <gene.doyel@cph.org>
2014-07-08 14:11:59 -0500

    [skip] Added additional tests for style name (1m)

--- spec/properties/style_name_spec.rb
+++ spec/properties/style_name_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 describe Rocx::Properties::StyleName do
   include ValuePropertyTestMacros
 
+  it_should_use tag: :name, name: "style_name", value: "Something"
+
   with_value("Something") do
     it_should_work
     it_should_output "<w:name w:val=\"Something\"/>"
