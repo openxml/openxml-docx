d7d3d7871b1eff40fdb8faa7594e11422278d451
Gene Doyel <gene.doyel@cph.org>
2014-07-24 13:35:50 -0500

    [improvement] Added the basedOn property to styles (3m) [#26c]

---
+++ lib/rocx/properties/parent_style.rb
@@ -0,0 +1,15 @@
+module Rocx
+  module Properties
+    class ParentStyle < ValueProperty
+      tag :basedOn
+
+      def valid?
+        value.is_a?(String) && value.length > 0
+      end
+
+      def invalid_message
+        "#{name} must be a string that is not empty"
+      end
+    end
+  end
+end
--- spec/style_spec.rb
+++ spec/style_spec.rb
@@ -92,6 +92,7 @@ describe Rocx::Style do
     it_should_have_value_property :hidden_style, with_value: true
     it_should_have_value_property :linked_style, with_value: "Test"
     it_should_have_value_property :locked, as_instance_of: :style_lock, with_value: true
+    it_should_have_value_property :parent_style, with_value: "Underline"
     it_should_have_value_property :personal, with_value: true
     it_should_have_value_property :personal_compose, with_value: true
     it_should_have_value_property :personal_reply, with_value: true
