903ad032d681a7594d2c1873725dda7c3f7a977d
Chase Clettenberg and Matthew Kobs <pair=chase.clettenberg+matt.kobs@cphepdev.com>
2015-11-19 09:16:06 -0600

    [improvement] Added remaining style properties (1h)

---
+++ lib/rocx/properties/string_property.rb
@@ -0,0 +1,15 @@
+module Rocx
+  module Properties
+    class StringProperty < ValueProperty
+
+      def valid?
+        value.is_a?(String) && !value.length.zero?
+      end
+
+      def invalid_message
+        "Invalid value for #{name}; string expected (provided: #{value.inspect})"
+      end
+
+    end
+  end
+end
--- lib/rocx/properties/style_name.rb
+++ lib/rocx/properties/style_name.rb
@@ -1,15 +1,7 @@
 module Rocx
   module Properties
-    class StyleName < ValueProperty
+    class StyleName < StringProperty
       tag :name
-
-      def valid?
-        value.is_a?(String) && !value.length.zero?
-      end
-
-      def invalid_message
-        "#{name} must be a non-empty string"
-      end
 
     end
   end
