4f75bac2c071b1353e2cd9411e464cb5d2062771
Chase Clettenberg <chase.clettenberg@cph.org>
2015-12-01 08:51:51 -0600

    [skip] Restructured folders to nest files with the same prefix (10m)

--- lib/openxml/docx/elements.rb
+++ lib/openxml/docx/elements.rb
@@ -8,6 +8,6 @@ end
 require "openxml/docx/elements/element"
 require "openxml/docx/elements/container"
 require "openxml/docx/elements/text"
-Dir.glob("#{File.join(File.dirname(__FILE__), "elements", "*.rb")}").each do |file|
+Dir.glob("#{File.join(File.dirname(__FILE__), "elements/**", "*.rb")}").each do |file|
   require file
 end
