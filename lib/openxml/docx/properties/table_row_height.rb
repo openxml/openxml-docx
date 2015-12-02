7511fd6a5499b7688df4890798abe55da549a5d2
Chase Clettenberg <chase.clettenberg@cph.org>
2015-12-02 16:02:00 -0600

    [skip] Correctly implemented table row height property (15m)

--- lib/openxml/docx/properties/table_row_height.rb
+++ lib/openxml/docx/properties/table_row_height.rb
@@ -1,14 +1,15 @@
 module OpenXml
   module Docx
     module Properties
-      class TrHeight < ComplexProperty
+      class TableRowHeight < ComplexProperty
+        tag :trHeight
 
         attribute :hRule, expects: :valid_hrule, namespace: :w
         attribute :val, expects: :positive_integer, namespace: :w
 
       private
         def valid_hrule(value)
-          list = %i(auto atLeast exact).include? value
+          list = %i(auto atLeast exact)
           message = "Invalid #{name}: must be one of #{list.join(", ")} (was #{value.inspect})"
           raise ArgumentError, message unless list.include? value
         end
