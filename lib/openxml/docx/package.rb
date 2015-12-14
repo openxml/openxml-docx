4f3696a020928464c2c0e385097299499410d244
Matthew Kobs <matt.kobs@cph.org>
2015-12-14 10:28:09 -0600

    [skip] Added Font Table part (3h)

--- lib/openxml/docx/package.rb
+++ lib/openxml/docx/package.rb
@@ -5,15 +5,19 @@ module OpenXml
     class Package < OpenXml::Package
       attr_reader :document,
                   :doc_rels,
+                  :font_rels,
                   :settings,
                   :headers,
                   :footers,
-                  :styles
+                  :styles,
+                  :fonts
 
       content_types do
         default "xml", TYPE_XML
+        default "odttf", TYPE_OBSCURED_FONT
         override "/word/styles.xml", TYPE_STYLES
         override "/word/settings.xml", TYPE_SETTINGS
+        override "/word/fontTable.xml", TYPE_FONT_TABLE
       end
 
       def initialize
@@ -21,19 +25,26 @@ module OpenXml
 
         rels.add_relationship REL_DOCUMENT, "/word/document.xml"
         @doc_rels = OpenXml::Parts::Rels.new
+        @font_rels = OpenXml::Parts::Rels.new
         @settings = OpenXml::Docx::Parts::Settings.new
         @styles = OpenXml::Docx::Parts::Styles.new
+        @fonts = OpenXml::Docx::Parts::Fonts.new
         @document = OpenXml::Docx::Parts::Document.new
         @headers = []
         @footers = []
 
         doc_rels.add_relationship REL_STYLES, "styles.xml"
         doc_rels.add_relationship REL_SETTINGS, "settings.xml"
+        doc_rels.add_relationship REL_FONT_TABLE, "fontTable.xml"
 
         add_part "word/_rels/document.xml.rels", doc_rels
+        add_part "word/_rels/fontTable.xml.rels", font_rels
         add_part "word/document.xml", document
         add_part "word/settings.xml", settings
         add_part "word/styles.xml", styles
+        add_part "word/fontTable.xml", fonts
+      end
+
       end
 
       def add_header(header)
---
+++ lib/openxml/docx/parts/fonts.rb
@@ -0,0 +1,35 @@
+module OpenXml
+  module Docx
+    module Parts
+      class Fonts < OpenXml::Part
+        include RootNamespaces
+
+        attr_reader :fonts
+
+        use_namespaces :r, :w
+
+        def initialize
+          @fonts = []
+        end
+
+        def <<(font)
+          @fonts << font
+        end
+
+        def count
+          fonts.count
+        end
+
+        def to_xml
+          build_standalone_xml do |xml|
+            xml.fonts(root_namespaces) {
+              xml.parent.namespace = xml.parent.namespace_definitions.find { |ns| ns.prefix == "w" }
+              fonts.each { |font| font.to_xml(xml) }
+            }
+          end
+        end
+
+      end
+    end
+  end
+end
