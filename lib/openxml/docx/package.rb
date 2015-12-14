fc171178dc1ff2e08fedc78417857b1ece35affb
Matthew Kobs <matt.kobs@cph.org>
2015-12-14 10:29:12 -0600

    [skip] Implemented font embedding in docx [#3803e] (3h)

--- lib/openxml/docx/package.rb
+++ lib/openxml/docx/package.rb
@@ -45,6 +45,22 @@ module OpenXml
         add_part "word/fontTable.xml", fonts
       end
 
+      def embed_truetype_font(path: nil, name: nil)
+        File.open(path, "rb") do |source_font|
+          obfuscation_data = obfuscate_font source_font
+          data = obfuscation_data[:bytes] << source_font.read
+          destination_font_name = "font#{fonts.fonts.count + 1}.odttf"
+          add_part "word/fonts/#{destination_font_name}", OpenXml::Parts::UnparsedPart.new(data)
+          font_relationship = font_rels.add_relationship REL_FONT, "fonts/#{destination_font_name}"
+
+          font_description = OpenXml::Docx::Elements::Font.new
+          font_description.font_name = name
+          embed_tag = OpenXml::Docx::Elements::EmbedRegular.new
+          embed_tag.font_key = "{#{obfuscation_data[:key]}}"
+          embed_tag.relationship_id = font_relationship.id
+          font_description << embed_tag
+          fonts << font_description
+        end
       end
 
       def add_header(header)
@@ -63,6 +79,29 @@ module OpenXml
         add_part "word/#{footer_name}", footer
         relationship = doc_rels.add_relationship REL_FOOTER, footer_name
         relationship.id
+      end
+
+    private
+
+      def obfuscate_font(font)
+        # From the OpenXml spec, section 17.8.1, the algorithm for obfuscating a font:
+        # - Generate a GUID, which is used and stored as the obfuscation key
+        # - Reverse the order of the bytes in the GUID (i.e. Big Endian ordering)
+        # - XOR the value with the first 32 bytes of the binary: once against 0-15, once against 16-31
+        # - Store the resulting file in the document, and store the obfuscation key in the fontKey attribute
+
+        key = SecureRandom::uuid.upcase # Spec requires hex characters be uppercase
+        raw_key = key.gsub("-", "")
+        big_endian_key = [raw_key].pack("H*").bytes.reverse
+        obfuscated_bytes = []
+        2.times do
+          bytes = font.read(16).bytes
+          (0...16).each do |index|
+            obfuscated_bytes << (bytes[index] ^ big_endian_key[index])
+          end
+        end
+
+        { key: key, bytes: obfuscated_bytes.pack("C*") }
       end
 
     end
