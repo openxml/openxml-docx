9bb99da77c21cfff17f34a120aa6ce9d926c5eca
Matthew Kobs <matt.kobs@cph.org>
2016-01-05 14:52:03 -0600

    [skip] Tweaked non-visual shape drawing properties (5m)

--- spec/elements/drawingml/non_visual_shape_properties_spec.rb
+++
@@ -1,8 +0,0 @@
-require "spec_helper"
-
-describe OpenXml::DrawingML::Elements::NonVisualShapeDrawingProperties do
-  include ElementTestMacros
-
-  it_should_use tag: :cNvSpPr, name: "non_visual_shape_drawing_properties"
-
-end
