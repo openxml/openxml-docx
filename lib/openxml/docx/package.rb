21f60cb95b98d51e60fc7c448835dbc81431c974
Matthew Kobs <matt.kobs@cph.org>
2016-01-12 11:27:51 -0600

    [skip] Implemented image object creator function (2h)

--- lib/openxml/docx/package.rb
+++ lib/openxml/docx/package.rb
@@ -1,4 +1,5 @@
 require "openxml/package"
+require "openxml/drawingml"
 require "open-uri"
 
 module OpenXml
@@ -67,7 +68,7 @@ module OpenXml
         end
       end
 
-      def embed_image(path: nil, content_type: nil, into_part: nil)
+      def embed_image(path: nil, content_type: nil, into_part: nil, rid_only: true, options: {})
         return if path.nil?
         into_part = document unless into_part.respond_to?(:relationships)
 
@@ -84,7 +85,7 @@ module OpenXml
           image_names << destination_image_name
 
           image_relationship = into_part.relationships.add_relationship REL_IMAGE, "media/#{destination_image_name}"
-          image_relationship.id
+          rid_only ? image_relationship.id : image(image_relationship.id, options: options)
         end
       end
 
@@ -129,6 +130,173 @@ module OpenXml
         end
 
         { key: key, bytes: obfuscated_bytes.pack("C*") }
+      end
+
+      def image(image_rid: nil, options: {})
+        return if image_rid.nil?
+
+        width = options.fetch(:width, 0)
+        height = options.fetch(:height, 0)
+
+        # This is the overall container for the drawing elements
+        drawing = OpenXml::Docx::Elements::Drawing.new
+
+        # This is the anchor that provides positioning information in Word
+        anchor = options[:anchor] ? OpenXml::Docx::Elements::WordProcessingDrawingAnchor.new : OpenXml::Docx::Elements::WordProcessingDrawingInline.new
+        anchor.distance_from_bottom = options.fetch(:db, 0)
+        anchor.distance_from_top = options.fetch(:dt, 0)
+        anchor.distance_from_left = options.fetch(:dl, 0)
+        anchor.distance_from_right = options.fetch(:dr, 0)
+
+        if options[:anchor]
+          anchor.allow_overlap = options.fetch(:allow_overlap, true)
+          anchor.behind_document = options.fetch(:behind_document, false)
+          anchor.layout_in_cell = options.fetch(:layout_in_cell, true)
+          anchor.locked = options.fetch(:locked, false)
+          anchor.z_index = options.fetch(:z_index, 1000)
+          anchor.simple_position = options.fetch(:use_simple_position, false)
+
+          # These elements are required to be children of anchor and supply the actual positioning information
+          simple_pos = OpenXml::Docx::Elements::WordProcessingDrawingSimplePosition.new
+          position = options.fetch(:position, [0, 0])
+          simple_pos.x = position[0]
+          simple_pos.y = position[1]
+          anchor << simple_pos
+
+          position_h = OpenXml::Docx::Elements::WordProcessingDrawingPositionH.new
+          position_h.relative_from = :column
+          position_offset = OpenXml::Docx::Elements::WordProcessingDrawingPositionOffset.new
+          position_offset.value = position[0].to_s
+          position_h << position_offset
+          anchor << position_h
+          position_v = OpenXml::Docx::Elements::WordProcessingDrawingPositionV.new
+          position_v.relative_from = :paragraph
+          position_offset = OpenXml::Docx::Elements::WordProcessingDrawingPositionOffset.new
+          position_offset.value = position[1].to_s
+          position_v << position_offset
+          anchor << position_v
+        end
+
+        # Extent is the size of the Object
+        extent = OpenXml::Docx::Elements::WordProcessingDrawingExtent.new
+        extent.extent_length = width
+        extent.extent_width = height
+        anchor << extent
+
+        if options[:anchor]
+          wrap = options.fetch(:wrap, { type: :none })
+          wrap_obj = case wrap[:type]
+          when :polygon
+            return nil unless wrap[:coordinates]
+            behavior_container = wrap[:behavior] == :through ? OpenXml::Docx::Elements::WordProcessingDrawingWrapThrough.new : OpenXml::Docx::Elements::WordProcessingDrawingWrapTight.new
+            behavior_container.distance_from_left = wrap.fetch(:distL, 0)
+            behavior_container.distance_from_right = wrap.fetch(:distR, 0)
+            behavior_container.wrap_text = wrap.fetch(:wrap_text, :bothSides)
+            polygon = OpenXml::Docx::Elements::WordProcessingDrawingWrapPolygon.new
+            wrap[:coordinates].each_with_index do |(x, y), index|
+              coordinate_type = index == 0 ? :start : :lineTo
+              coordinate = OpenXml::Docx::Elements::WordProcessingDrawingWrapCoordinate.new(coordinate_type)
+              coordinate.x = x
+              coordinate.y = y
+              polygon << coordinate
+            end
+            behavior_container << polygon
+            behavior_container
+          when :square
+            square = OpenXml::Docx::Elements::WordProcessingDrawingWrapSquare.new
+            square.distance_from_bottom = wrap.fetch(:distB, 0)
+            square.distance_from_left = wrap.fetch(:distL, 0)
+            square.distance_from_right = wrap.fetch(:distR, 0)
+            square.distance_from_top = wrap.fetch(:distT, 0)
+            square.wrap_text = wrap.fetch(:wrap_text, :bothSides)
+            square
+          when :top_and_bottom
+            top_bottom = OpenXml::Docx::Elements::WordProcessingDrawingWrapTopAndBottom.new
+            top_bottom.distance_from_bottom = wrap.fetch(:distB, 0)
+            top_bottom.distance_from_top = wrap.fetch(:distT, 0)
+            top_bottom
+          else
+            nil
+          end
+          anchor << (wrap_obj || OpenXml::Docx::Elements::WordProcessingDrawingWrapNone.new)
+        end
+
+        # Object (non-visual) properties are also required
+        doc_pr = OpenXml::Docx::Elements::WordProcessingDrawingObjectNvProperties.new
+        doc_pr.description = options[:filename] || options[:name] || "Image"
+        doc_pr.object_name = options[:name] || "Image"
+        doc_pr.id = @image_names.count + 1
+        anchor << doc_pr
+
+        # This is the actual graphics object
+        graphic = OpenXml::DrawingML::Elements::Graphic.new
+        graphic_data = OpenXml::DrawingML::Elements::GraphicData.new
+        graphic_data.uri = "http://schemas.openxmlformats.org/drawingml/2006/picture"
+        pic_container = OpenXml::DrawingML::Elements::Picture.new
+
+        # Must also define the Picture's non-visual Properties
+        pic_nv_properties = OpenXml::DrawingML::Elements::NonVisualPictureProperties.new
+        nv_properties = OpenXml::DrawingML::Elements::NonVisualDrawingProperties.new
+        nv_properties.id = @image_names.count + 1
+        nv_properties.picture_name = options[:filename] || options[:name] || "Image"
+        nv_properties.description = options[:name] || "Image"
+        pic_nv_properties << nv_properties
+        drawing_properties = OpenXml::DrawingML::Elements::NonVisualPictureDrawingProperties.new
+        picture_locks = OpenXml::DrawingML::Elements::PictureLocks.new
+        picture_locks.disallow_aspect_ratio_changes = true
+        picture_locks.disallow_arrowhead_changes = true
+        drawing_properties << picture_locks
+        pic_nv_properties << drawing_properties
+        pic_container << pic_nv_properties
+
+        # Blip Fill defines the picture that will fill the object's shape
+        blip_fill = OpenXml::DrawingML::Elements::BlipFill.new
+        pic_ref = OpenXml::DrawingML::Elements::Blip.new
+        pic_ref.embed = image_rid
+        stretch = OpenXml::DrawingML::Elements::Stretch.new
+        fill_rect = OpenXml::DrawingML::Elements::FillRectangle.new
+        stretch << fill_rect
+        blip_fill << pic_ref
+        blip_fill << stretch
+        blip_fill << OpenXml::DrawingML::Elements::SourceRectangle.new
+        pic_container << blip_fill
+
+        # Must also define the picture's shape properties
+        shape_properties = OpenXml::DrawingML::Elements::PictureShapeProperties.new
+        shape_properties.black_and_white_mode = :auto
+        transform = OpenXml::DrawingML::Elements::TransformEffect.new
+        offset = OpenXml::DrawingML::Elements::Offset.new
+        offset.x = 0
+        offset.y = 0
+        transform << offset
+        extent = OpenXml::DrawingML::Elements::Extents.new
+        extent.extent_length = width
+        extent.extent_width = height
+        transform << extent
+        shape_properties << transform
+
+        preset_geometry = OpenXml::DrawingML::Elements::PresetGeometry.new
+        preset_geometry.preset = :rect
+        adj_values_list = OpenXml::DrawingML::Elements::AdjustValuesList.new
+        preset_geometry << adj_values_list
+        shape_properties << preset_geometry
+        no_fill = OpenXml::DrawingML::Elements::NoFill.new
+        shape_properties << no_fill
+
+        outline = OpenXml::DrawingML::Elements::Outline.new
+        outline << no_fill
+
+        shape_properties << outline
+
+        pic_container << shape_properties
+
+        graphic_data << pic_container
+        graphic << graphic_data
+
+        anchor << graphic
+        drawing << anchor
+
+        drawing
       end
 
     end
